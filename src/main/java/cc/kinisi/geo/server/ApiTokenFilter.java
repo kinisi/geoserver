2ff6f400dff94a6765b2459587648ce7db84c7db
Adam Ohren <adam.ohren@gmail.com>
2014-04-11 02:10:57 -0400

    Added initial logging for ApiTokenFilter. Need to figure out how to get source IP from load balancer; REMOTE_ADDR header ain't doing it.

--- src/main/java/cc/kinisi/geo/server/ApiTokenFilter.java
+++ src/main/java/cc/kinisi/geo/server/ApiTokenFilter.java
@@ -12,10 +12,17 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import cc.kinisi.geo.data.ApiToken;
 
 public class ApiTokenFilter implements Filter {
 
+  private Logger logger = LoggerFactory.getLogger(ApiTokenFilter.class);
+    
+  public static final String REMOTE_ADDR_HEADER = "REMOTE_ADDR";
+  
 	public static final String TOKEN_HEADER_NAME = "X-Api-Token";
 	public static final String TOKEN_PARAM_NAME = "api_token";
 			
@@ -42,15 +49,22 @@ public class ApiTokenFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
 	  
+	  String balancerIp = request.getRemoteAddr();
 		if (request instanceof HttpServletRequest && response instanceof HttpServletResponse) {
-			String tokenString = getTokenValue(((HttpServletRequest) request));
+		  HttpServletRequest httpReq = (HttpServletRequest) request;
+		  String sourceIp = httpReq.getHeader(REMOTE_ADDR_HEADER);
+			String tokenString = getTokenValue(httpReq);
 			ApiToken apiToken = controller.getApiToken(tokenString);
 			if (apiToken != null && apiToken.isValid()) {
+			  logger.debug("Validated API token {} from source IP {} via {}", tokenString, sourceIp, balancerIp);
 			  request.setAttribute(API_TOKEN_REQUEST_KEY, apiToken);			  
 				chain.doFilter(request, response);
 			} else {
+			  logger.warn("Rejected API token {} from source IP {} via {}", tokenString, sourceIp, balancerIp);
 				((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED);
 			}
+		} else {
+		  logger.error("ServletRequest/ServletResponse not of HTTP type!");
 		}
 
 	}
