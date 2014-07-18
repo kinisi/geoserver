054da80cf0726df730237b9ab27b5f05900c8819
Adam Ohren <adam.ohren@gmail.com>
2014-07-18 01:16:20 -0400

    Also include measure_time in SQL script for deduping.

--- sql/kinisi_deviceloc_dedupe.sql
+++ sql/kinisi_deviceloc_dedupe.sql
@@ -10,15 +10,13 @@ CREATE TABLE `device_location_unique` (
   `speed` double DEFAULT NULL,
   `track` double DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `device_lat_long` (`device_id`,`latitude`,`longitude`),
-  KEY `device_time` (`device_id`,`measure_time`)
+  UNIQUE KEY `device_time_lat_long` (`device_id`,`measure_time`,`latitude`,`longitude`)
 ) ENGINE=InnoDB AUTO_INCREMENT=606811 DEFAULT CHARSET=utf8;
 
-
-create temporary table dmax select device_id, latitude, longitude, max(id) max_id from device_location group by 1, 2, 3;
+create temporary table dmax select device_id, measure_time, latitude, longitude, max(id) max_id from device_location group by 1, 2, 3, 4;
 
 insert into device_location_unique
-select t1.altitude, t1.climb, t1.device_id, t1.id, t1.latitude, t1.longitude, t1.measure_time, t1.receive_time, t1.speed, t1.track from device_location t1 join dmax t2 on t1.id = t2.max_id 
+select t1.altitude, t1.climb, t1.device_id, t1.id, t1.latitude, t1.longitude, t1.measure_time, t1.receive_time, t1.speed, t1.track from device_location t1 join dmax t2 on t1.id = t2.max_id;
 
 rename table device_location to device_location_dupes;
 rename table device_location_unique to device_location;
